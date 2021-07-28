48837ee816a92e6971b67be3f4cb625725899b6a
Web Zahidul <webzahidul@gmail.com>
2021-07-29 00:59:42 +0600

    factorial-done

--- factorial.js
+++ factorial.js
@@ -1,9 +1,12 @@
 function factorial(n){
-    var factorial = 1;
+    var fact = 1;
 for (var i = 1; i <= n; i++) {
-    factorial = factorial * i;
+    fact = fact * i;
 }
-return factorial;
+return fact;
 }
 var result = factorial(10);
 console.log(result);
+
+//jog korar somoy initial value hobe 0,
+//gun korar somoy initial value hobe 1.
